40cf9c567687f39bfa6a16e1f18403295a79fe1a
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-08 23:44:10 +0300

    Added add_client function

--- Server/include/ClientManager.hpp
+++ Server/include/ClientManager.hpp
@@ -3,10 +3,17 @@
 #include <iostream>
 #include <unordered_map>
 #include <boost/asio.hpp>
+#include <Client.hpp>
 
 class ClientManager {
     using tcp = boost::asio::ip::tcp;
 
+private:
+    std::unordered_map<std::string, Client> clients_;
+
 public:
     ClientManager();
+
+    void add_client(const std::string& ip_addr, std::shared_ptr<tcp::socket> socket);
+    void check_message();
 };
--- Server/src/core/ClientManager.cpp
+++ Server/src/core/ClientManager.cpp
@@ -1,5 +1,13 @@
 #include <ClientManager.hpp>
 
 ClientManager::ClientManager() {
-    std::cout << "Client manager.\n";
+    check_message();
+}
+
+void ClientManager::add_client(const std::string& ip_addr, std::shared_ptr<tcp::socket> socket) {
+    clients_[ip_addr] = Client{std::move(socket)};
+}
+
+void ClientManager::check_message() {
+
 }
