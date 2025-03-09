4ec41635daafbc0c396b8fa8869307306648be7b
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-09 19:44:25 +0300

    Added logic for simple connect

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
 #include <boost/asio.hpp>
 
 int main() {
+    using boost::asio::ip::tcp;
+    boost::asio::io_context io_context;
+    tcp::socket socket(io_context);
+    tcp::endpoint ep(boost::asio::ip::make_address_v4("92.101.12.97"), 12345);
+    boost::system::error_code err;
+    socket.connect(ep, err);
+    if (!err) {
+        std::cout << "Connected\n";
+    } else {
+        std::cerr << "Error!\n";
+    }
+
     return 0;
 }
