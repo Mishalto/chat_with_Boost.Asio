66ef3f1b2f1b8f9057bd7d364d6a6e8832e29fde
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-09 21:34:50 +0300

    add_client has been improved

--- Server/src/core/ClientManager.cpp
+++ Server/src/core/ClientManager.cpp
@@ -9,8 +9,13 @@ void ClientManager::check_message() {
 }
 
 void ClientManager::add_client(const std::string& ip_addr, std::shared_ptr<tcp::socket> socket) {
-    std::cout << "[ClientManager]" << ip_addr << " added.\n";
-    clients_[ip_addr] = Client{socket};
+    auto find_it = clients_.find(ip_addr);
+    if (find_it == clients_.end()) {
+        std::cout << "[ClientManager]" << ip_addr << " added.\n";
+        clients_[ip_addr] = Client{socket};
+    } else {
+        std::cout << "[ClientManager]" << ip_addr << " already here.\n";
+    }
 }
 
 size_t ClientManager::active_client() const {
