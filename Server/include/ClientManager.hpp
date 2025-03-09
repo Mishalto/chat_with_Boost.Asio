790306139c7364aafe6c21f2d4210b39a699511a
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-09 22:22:27 +0300

    check mess logic added

--- Server/include/ClientManager.hpp
+++ Server/include/ClientManager.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <unordered_map>
+#include <thread>
 #include <boost/asio.hpp>
 #include <Client.hpp>
 
@@ -15,6 +16,6 @@ private:
 public:
     ClientManager();
 
-    void add_client(const std::string& ip_addr, std::shared_ptr<tcp::socket> socket);
+    void add_client(const std::string& ip_addr, std::shared_ptr<tcp::socket>& socket);
     size_t active_client() const;
 };
--- Server/src/core/ClientManager.cpp
+++ Server/src/core/ClientManager.cpp
@@ -1,14 +1,22 @@
 #include <ClientManager.hpp>
 
 ClientManager::ClientManager() {
-    check_message();
+    std::thread check_thread(&ClientManager::check_message, this);
+    check_thread.detach();
 }
 
 void ClientManager::check_message() {
-    //todo
+    for (;;) {
+        for (auto& s : clients_) {
+            if (s.second.has_message()) {
+                std::cout << s.second.get_message();
+                s.second.reset_flag();
+            }
+        }
+    }
 }
 
-void ClientManager::add_client(const std::string& ip_addr, std::shared_ptr<tcp::socket> socket) {
+void ClientManager::add_client(const std::string& ip_addr, std::shared_ptr<tcp::socket>& socket) {
     auto find_it = clients_.find(ip_addr);
     if (find_it == clients_.end()) {
         std::cout << "[ClientManager]" << ip_addr << " added.\n";
